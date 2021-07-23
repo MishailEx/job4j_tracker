cf4cb496e0332a89a45cdc001dc05bd3c33d26ac
MishailEx <Mishail.avram@mail.ru>
2021-07-23 13:45:57 +0300

    1.7. Конструктор [#173889]

---
+++ src/test/java/ru/job4j/oop/Error.java
@@ -0,0 +1,27 @@
+package ru.job4j.oop;
+
+public class Error {
+    boolean active;
+    int status;
+    String message;
+
+    public Error() {
+    }
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public void show() {
+        System.out.println(active + " " + status + " " + message);
+    }
+
+    public static void main(String[] args) {
+        Error error = new Error();
+        Error error1 = new Error(true, 2, "Ok");
+        error.show();
+        error1.show();
+    }
+}
