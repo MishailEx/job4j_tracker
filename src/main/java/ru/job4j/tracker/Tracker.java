c99745c27324fa1d95bf4a7dc159df183f9841a9
MishailEx <Mishail.avram@mail.ru>
2021-07-25 08:56:08 +0300

    6. Метод замены заявки. Tracker.replace [#211748 #181132]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -15,8 +15,7 @@ public class Tracker {
     }
 
     public Item findById(int id) {
-        int index = indexOf(id);
-        return index != -1 ? items[index] : null;
+        return indexOf(id) != -1 ? items[indexOf(id)] : null;
     }
 
     private int indexOf(int id) {
