be2fb8c4e6beddf79a9ceafec285c94aa4cc9ed0
MishailEx <Mishail.avram@mail.ru>
2021-07-26 13:48:09 +0300

    5.1. Тестирование. Подготовка данных. [#182960 #181665]

--- src/test/java/ru/job4j/tracker/TrackerTest.java
+++ src/test/java/ru/job4j/tracker/TrackerTest.java
@@ -2,6 +2,7 @@ package ru.job4j.tracker;
 
 import org.junit.Test;
 import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertThat;
 import static org.hamcrest.core.IsNull.nullValue;
 
@@ -85,6 +86,6 @@ public class TrackerTest {
         tracker.add(bug);
         int id = bug.getId();
         tracker.delete(id);
-        assertThat(tracker.findById(id), is(nullValue()));
+        assertNull(tracker.findById(bug.getId()));
     }
 }
