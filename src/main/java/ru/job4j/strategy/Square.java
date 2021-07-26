c488bb11f16c0b6568b6c90798beba10138be8a9
MishailEx <Mishail.avram@mail.ru>
2021-07-26 13:50:02 +0300

    7. Шаблон проектирования - Стратегия [#786 #181681]

--- src/main/java/ru/job4j/strategy/Square.java
+++ src/main/java/ru/job4j/strategy/Square.java
@@ -9,7 +9,4 @@ public class Square implements Shape {
                 + "|______|" + ln;
     }
 
-    public static void main(String[] args) {
-        System.out.println(new Square().draw());
-    }
 }
