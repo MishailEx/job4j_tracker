0c5aa4c0957c45bd9bc2c85a60ad93bacaff41e1
MishailEx <Mishail.avram@mail.ru>
2021-07-27 09:22:44 +0300

    4. final [#334073]

---
+++ src/main/java/ru/job4j/ooa/Airbus.java
@@ -0,0 +1,34 @@
+package ru.job4j.ooa;
+
+public final class Airbus extends Aircraft {
+    private static final int COUNT_ENGINE = 2;
+
+    private String name;
+
+    public Airbus(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void printCountEngine() {
+        int countEngine = COUNT_ENGINE;
+        if (this.name.equals("A380")) {
+            countEngine += 2;
+        }
+        System.out.println("Количество двигателей равно: " + countEngine);
+    }
+
+    @Override
+    public String toString() {
+        return "Airbus{"
+                + "name='" + name + '\''
+                + '}';
+    }
+}
