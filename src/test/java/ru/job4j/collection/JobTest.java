f9a54192973686c9411e403aac79a9348b1e4fcc
MishailEx <Mishail.avram@mail.ru>
2021-08-02 13:16:35 +0300

    2. Комбинированный компаратор [#10036 #184081]

--- src/test/java/ru/job4j/collection/JobTest.java
+++ src/test/java/ru/job4j/collection/JobTest.java
@@ -14,9 +14,9 @@ public class JobTest {
                 thenComparing(new SortedReverseByPriority());
         int rsl = cmpNamePriority.compare(
                 new Job("Impl task", 0),
-                new Job("Fix bug", 1)
+                new Job("Impl task", 1)
         );
-        assertThat(rsl, lessThan(0));
+        assertThat(rsl, is(1));
     }
 
     @Test
