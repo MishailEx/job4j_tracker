49f305c3871926003a89aeb4fd20687efb1e1ed5
MishailEx <Mishail.avram@mail.ru>
2021-08-03 12:47:57 +0300

    5. Подсчет функции в диапазоне. [#24251]

--- src/main/java/ru/job4j/function/FunctionProb.java
+++ src/main/java/ru/job4j/function/FunctionProb.java
@@ -2,16 +2,21 @@ package ru.job4j.function;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Supplier;
 
 public class FunctionProb {
-   public static List<Double> diapason(int start, int end, Function<Double, Double> func) {
-      Supplier<List<Double>> array = ArrayList::new;
-      List<Double> list = array.get();
-      for (double i = start; i < end; i++) {
-         list.add(func.apply(i));
-      }
-      return list;
-   }
+    public static List<Double> diapason(int start, int end, Function<Double, Double> func) {
+        Supplier<List<Double>> array = ArrayList::new;
+        List<Double> list = array.get();
+        BiConsumer<Integer, Integer> con = (i, i2) -> {
+            for (double j = i; j < i2; j++) {
+                list.add(func.apply(j));
+            }
+        };
+        con.accept(start, end);
+        return list;
+    }
 }
