604f7478b6bc847a172cde5c8bb772a6b151a546
MishailEx <Mishail.avram@mail.ru>
2021-08-03 11:01:11 +0300

    2.2. Лямбда блок [#249208]

---
+++ src/main/java/ru/job4j/function/LambdaUsage.java
@@ -0,0 +1,13 @@
+package ru.job4j.function;
+
+import java.util.Comparator;
+
+public class LambdaUsage {
+    public static void main(String[] args) {
+        Comparator<String> cmpText = (left, right) -> left.compareTo(right);
+        Comparator<String> cmpDescSize = (left, right) -> {
+            System.out.println("compare - " + right.length() + " : " + left.length());
+            return right.length() - left.length();
+        };
+    }
+}
