d5ba749d0f5f2871ab394de78f4a9f6c7cb0dbe4
MishailEx <Mishail.avram@mail.ru>
2021-08-04 19:31:11 +0300

    2.2. Лямбда блок [#249208 #185216]

--- src/main/java/ru/job4j/function/LambdaUsage.java
+++ src/main/java/ru/job4j/function/LambdaUsage.java
@@ -1,13 +1,20 @@
 package ru.job4j.function;
 
+import java.util.Arrays;
 import java.util.Comparator;
+import java.util.List;
 
 public class LambdaUsage {
     public static void main(String[] args) {
+        List<String> str = Arrays.asList("Vlad", "Boris", "Oleg");
         Comparator<String> cmpText = (left, right) -> left.compareTo(right);
-        Comparator<String> cmpDescSize = (left, right) -> {
+                Comparator<String> cmpDescSize = (left, right) -> {
             System.out.println("compare - " + right.length() + " : " + left.length());
-            return right.length() - left.length();
+            return Integer.compare(right.length(), left.length());
         };
+        str.sort(cmpText);
+        str.forEach(System.out::println);
+        str.sort(cmpDescSize);
+        str.forEach(System.out::println);
     }
 }
